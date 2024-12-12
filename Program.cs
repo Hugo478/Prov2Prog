d2283b204d717e8583c6c5aa52537e0f24891bd8
TE22E-Sundstrom-Hugo <hugo.sundstrom@elev.ga.ntig.se>
2024-12-12 11:18:44 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq.Expressions;
+
+while (true)
+{
+    
+    Console.WriteLine("Hur många bilar ska skapas? ");
+Console.WriteLine("Skriv 0 för att avsluta programmet");
+    string SkapaSvar =  Console.ReadLine();
+    int IntSvar = int.Parse(SkapaSvar);
+    
+    if (IntSvar >= 1)
+    {
+        Console.WriteLine($"Du har skapat "+IntSvar+" bilar");
+        List<int> Bilar;
+    }
+    if (IntSvar == 0)
+    {
+        return;
+    }
+    else 
+    {
+        Console.WriteLine("Skriv hur många i bilar som skapas med siffror (mer än 0) ");
+    }
+}
